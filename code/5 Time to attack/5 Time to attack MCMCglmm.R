649b17023a1ad07e59b6bab2d55b48f71c29f685
Mads <abumadsen@gmail.com>
2021-10-20 13:19:46 +0200

    Changed from N attackers analysis to time till attack

--- code/5 Time to attack/5 Time to attack MCMCglmm.R
+++ code/5 Time to attack/5 Time to attack MCMCglmm.R
@@ -1,6 +1,13 @@
 #30/06/20
 #Mads F. Schou
 
+#Lena comments:
+# det gik lige op for mig at vi aldrig testede om der var forskel paa arterne i hvor lang tid det tog dem at angribe! Kan du koere saadan en test? Den burde vaere simplere fordi vi ikke behoever tids-aspekted som predictor. Efter et hurtigt plot i excel synes jeg ikke det ser ud som om der er meget forskel mellem arterne i deres respons til forskelligt str bytte, men det er en test der er vaerd at koere (jeg ved ikke lige hvorfor vi ikke har taenkt paa det noget foer!)
+# 
+# Det skal dog lige siges at der altsaa er en del angreb der aabenbart sker efter de standardiserede 10min, saa disse skal nok udelades fra denne analyse. 
+# Jeg tror vi valgte at inkl. dem i hoved-analysen fordi vi jo der er mest interesserede i hvor mange gruppemedlemmer der angriber over tid, naar foerst byttet er bidt. (Men jeg skal vist lige inkludere lidt detaljer om det i metode-sektionen og ogsaa omregne mine overall acceptance rates). Nogle kolonier angriber indenfor 11min, og vi noterede dem naar vi kunne se aktivitet i reden omkring 9-10min og kunne se de nok snart ville angribe. Saa maaske prey acceptance skal oeges til 11min? Jeg kan faktisk ikke lige huske hvad vi gjorde med disse i DGAE paper, kan du? Saa vidt jeg umiddelbart kan se fra DGAE data saa kaldte vi et prey accepted op til 13 min, men jeg ved ikke om du aendrede paa det i analysen i R. 
+
+
 pacman::p_load("dplyr","tidyr","doBy","MCMCglmm","parallel","coda","fitR")
 
 DATAPATH = "interm"
@@ -12,23 +19,28 @@ mydat <- read.table(paste(DATAPATH,"social attack data for Mads 22.05.20_prepped
 #---- Prior
 MyPrior <- list(
   R=list(V = diag(1), nu=0.002, fix = FALSE),
-  G=list(G1=list(V        = diag(2),
-                 nu        = 1.002)))
-
-MyPrior.nest <- list(
-  R=list(V = diag(1), nu=0.002, fix = FALSE),
-  G=list(G1=list(V        = diag(2),
-                 nu        = 1.002),
-         G2=list(V        = diag(1),
+  G=list(G1=list(V        = diag(1),
                  nu        = 0.002)))
 
 ####################################
 ##---- Filtering (Keep minor at this stage)
 ####################################
 
-#Two highly influenctial
-outlier_ids <- mydat$trial[mydat$attackers > 20]
-mydat <- mydat[!mydat$trial %in% outlier_ids,]
+#Only one record per trial as we want to test time till first attack
+mydat <- unique(mydat[, c("species","nestId","preysize","LatencyToAttackSec","trial","species2")])
+mydat <- mydat[!is.na(mydat$LatencyToAttackSec),]
+nrow(mydat) #192
+
+#Confirm that only one obsevation per trial
+mydat$trial.nest <- paste(mydat$trial,mydat$nestId, sep = "_")
+unique(table(mydat$trial.nest)) #1 -> confirmed!
+table(table(mydat$nestId)) #But some nests are tested multiple times, so need this as a random effect
+
+#Removing time points after 10 min da det ikke længere kan ses som et attack
+mydat <- mydat[mydat$LatencyToAttackSec < 10*60,]
+nrow(mydat) #178
+
+
 
 ####################################
 ##---- Prepping (Keep minor at this stage)
@@ -36,19 +48,16 @@ mydat <- mydat[!mydat$trial %in% outlier_ids,]
 
 mydat <- mydat %>%
   mutate(preysize_z = scale(preysize)[,1]) %>%
-  mutate(timepoint_z = scale(timepoint)[,1]) %>%
-  mutate_at(c("species","species2","nestId","trial"), list(~factor(.))) %>%
-  mutate(preysize.log = log(preysize)) %>%
-  mutate(preysize.log_z = scale(preysize.log)[,1])
+  mutate_at(c("species","species2","nestId","trial"), list(~factor(.)))
 
 
 ####################################
 ##---- TEST RUN
 ####################################
-# 
+# # 
 # MyStart <- Sys.time()
-# mtest <- MCMCglmm(attackers ~ timepoint_z + species-1 + species:preysize_z,
-#                   random = ~ us(1+timepoint_z):trial,
+# mtest <- MCMCglmm(LatencyToAttackSec ~ species-1 + species:preysize_z,
+#                   random = ~ nestId,
 #                   data   = mydat,
 #                   family = "poisson",
 #                   prior  = MyPrior,
@@ -89,61 +98,27 @@ mydat <- mydat %>%
 ####################################
 
 
-m1_3.3way.nestid <- mclapply(1:3, function(i) {
-  MCMCglmm(attackers ~ species-1 + species:timepoint_z+ species:preysize_z+species:preysize_z:timepoint_z,
-           random = ~ us(1+timepoint_z):trial + nestId,
-           data   = mydat,
-           family = "poisson",
-           prior  = MyPrior.nest,
-           #thin   = 1,burnin = 0,nitt   = 1000,
-           nitt=2500000, thin=2500, burnin=100000,
-           verbose = T)
-}, mc.cores=3)
-
 m1_3.2way.nestid <- mclapply(1:3, function(i) {
-  MCMCglmm(attackers ~ species-1 + species:timepoint_z+ species:preysize_z+preysize_z:timepoint_z,
-           random = ~ us(1+timepoint_z):trial + nestId,
+  MCMCglmm(LatencyToAttackSec ~ species-1 + species:preysize_z,
+           random = ~  nestId,
            data   = mydat,
            family = "poisson",
-           prior  = MyPrior.nest,
+           prior  = MyPrior,
            #thin   = 1,burnin = 0,nitt   = 1000,
            nitt=2500000, thin=2500, burnin=100000,
            verbose = T)
 }, mc.cores=3)
 
 m1_3.1way.nestid <- mclapply(1:3, function(i) {
-  MCMCglmm(attackers ~ species-1 + preysize_z+timepoint_z,
-           random = ~ us(1+timepoint_z):trial + nestId,
+  MCMCglmm(LatencyToAttackSec ~ species-1 + preysize_z,
+           random = ~ nestId,
            data   = mydat,
            family = "poisson",
-           prior  = MyPrior.nest,
+           prior  = MyPrior,
            #thin   = 1,burnin = 0,nitt   = 1000,
            nitt=2500000, thin=2500, burnin=100000,
            verbose = T)
 }, mc.cores=3)
 
 
-
-####################################
-##---- FINAL RUN FOR PLOTTING
-####################################
-#This is necessary as I cannot get predict to run with the random slopes
-
-MyPriorPlot <- list(
-  R=list(V = diag(1), nu=0.002, fix = FALSE),
-  G=list(G1=list(V        = diag(1),
-                 nu        = 0.002),
-         G2=list(V        = diag(1),
-                 nu        = 0.002)))
-
-m1.3wayPlot <- MCMCglmm(attackers ~ species-1 + species:timepoint_z+ species:preysize_z+species:preysize_z:timepoint_z,
-                        random = ~ trial + nestId,
-                        data   = mydat,
-                        family = "poisson",
-                        prior  = MyPriorPlot,
-                        #thin   = 1,burnin = 0,nitt   = 1000,
-                        nitt=2500000, thin=2500, burnin=100000,
-                        verbose = T)
-
-
-save.image(paste(OUTPATH,"/", "N attackers no outliers.nest.RData", sep = ""))
+save.image(paste(OUTPATH,"/", "Time to attack.RData", sep = ""))
